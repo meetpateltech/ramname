52017b89891f5f50908a6483cbfda555d27a958b
Meet Patel <113581962+meetpateltech@users.noreply.github.com>
2024-01-21 23:05:52 +0530

    Update Disclaimer.svelte

--- src/lib/Disclaimer.svelte
+++ src/lib/Disclaimer.svelte
@@ -1,5 +1,5 @@
 <div class="space-y-2">
-  <p class="text-xl text-orange-600 uppercase">JAI SHREE RAM</p>
+  <p class="text-2xl text-orange-600 uppercase">JAI SHREE RAM</p>
   <a
     class="text-orange-600 underline block text-center"
     href="https://github.com/meetpateltech/ramname"
